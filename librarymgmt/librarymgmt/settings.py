63552430dfaee9e7c831fe8dca980c40763d03e6
Ayush Kumar <akmar1511@gmail.com>
2022-07-08 15:36:52 +0530

    Added dotenv file loading

--- librarymgmt/librarymgmt/settings.py
+++ librarymgmt/librarymgmt/settings.py
@@ -11,16 +11,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import dotenv_values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# dotenv values
+config = dotenv_values(Path.joinpath(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6o+lw_3_^&h@jq#td=id3)%^ksw1l+lz%=l8+d16lx+*x9x+33'
+SECRET_KEY = config['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
